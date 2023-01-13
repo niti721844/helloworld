ca44497b487d64579f162ac046bf9ffa06e80332
niti721844 <122540264+niti721844@users.noreply.github.com>
2023-01-13 08:44:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Heart)
