78ac95f63fad08e3d38b3d9077f4edcc73b74ebf
niti721844 <122540264+niti721844@users.noreply.github.com>
2023-01-13 08:59:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
 })
-basic.showIcon(IconNames.Heart)
+music.playTone(880, music.beat(BeatFraction.Breve))
